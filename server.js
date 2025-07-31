f6d292d1caab4cf29471142ab2e5dc6bd22653f8
JaysonPye <jaysonpye@hotmail.com>
2025-07-31 11:28:28 +0900

    security additions

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// server/server.js
-
 const express = require('express');
 const path = require('path'); 
 const enforce = require('express-sslify');
@@ -8,17 +6,22 @@ const port = process.env.PORT || 5000;
 const apiRoutes = require('./routes/api');
 const helmet = require('helmet');
 
+app.disable('x-powered-by');
+
+// Base helmet middleware to apply standard security headers (like nosniff)
+app.use(helmet());
+
+// Custom CSP
 app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "'unsafe-inline'", "https:"],
-      styleSrc: ["'self'", "'unsafe-inline'", "https:"],
+      scriptSrc: ["'self'", "https:"],
+      styleSrc: ["'self'", "https:"],
       imgSrc: [
         "'self'",
         "data:",
-        "https:",
         "https://lh3.googleusercontent.com",
         "https://drive.google.com"
       ],
@@ -30,35 +33,41 @@ app.use(
       fontSrc: ["'self'", "https:", "data:"],
       connectSrc: ["'self'", "https:"],
       objectSrc: ["'none'"],
+      workerSrc: ["'none'"],
+      formAction: ["'self'"],
+      frameAncestors: ["'none'"],
       upgradeInsecureRequests: [],
     },
   })
 );
 
+
+// Frameguard to prevent clickjacking
 app.use(helmet.frameguard({ action: 'deny' }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(express.static(path.join(__dirname, './client/build')));
 
-// Forbidden paths to satisy security scans even though they don't exist.
-
+// 404 paths to satisfy security scans even though they don't exist.
 app.use((req, res, next) => {
-  if (/^\/\.(git|hg|bzr|darcs|bitkeeper)(\/|$)/.test(req.path)) {
-    return res.status(403).send('Forbidden');
+  if (/^\/(\.git|\.hg|\.bzr|\.darcs|\.bitkeeper|_darcs|BitKeeper)(\/|$)/i.test(req.path)) {
+    return res.status(404).send('Not found');
   }
   next();
 });
 
+
 app.use('/api', apiRoutes);
 
-// enable HTTPS redirection in a secure environment
+// Enable HTTPS redirection in a secure environment
 if (process.env.NODE_ENV === 'production') {
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(helmet.hsts({ maxAge: 63072000, includeSubDomains: true, preload: true }));
 }
 
+// React SPA fallback
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
